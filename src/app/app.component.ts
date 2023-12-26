f1843992284a9e804f46eac05fe3432a7c96ec0e
romanostd <romanostedile@gmail.com>
2023-12-26 20:02:05 -0300

    feat: timeline and some styles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio-web';
+
+export class AppComponent implements OnInit{
+
+  @ViewChild('timelineWrapper') timelineWrapper!: ElementRef;
+
+  public timelineItems = [
+    {
+      date: '2018',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      date: '2019',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      date: '2020',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      date: '2021',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      date: '2021',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+
+    {
+      date: '2021',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+
+    {
+      date: '2021',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  ];
+
+  scrollLeft() {
+    this.timelineWrapper.nativeElement.scrollBy({ left: -250, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.timelineWrapper.nativeElement.scrollBy({ left: 250, behavior: 'smooth' }); 
+  }
+
+
+  title?: string;
+  private texts: string[] = ['Romano Marcos Stedile', 'Full Stack Developer'];
+  private currentIndex = 0;
+
+  ngOnInit(): void {
+    this.title = this.texts[this.currentIndex];
+
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.texts.length;
+      this.title = this.texts[this.currentIndex];
+    }, 6000); 
+  }
 }
