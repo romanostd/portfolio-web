ec5223855057fff04d70a5df45fa036e0f8ebb90
romanostd <romanostedile@gmail.com>
2024-01-05 14:36:09 -0300

    paths for image and translations updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
--- src/app/mocks/projects.mock.ts
+++ src/app/mocks/projects.mock.ts
@@ -1,27 +1,27 @@
 export const projects = [
   {
-    images: ['../assets/4asset.png', '../assets/4asset2.png', '../assets/4asset3.png'],
+    images: ['assets/4asset.png', 'assets/4asset2.png', 'assets/4asset3.png'],
     title: 'PROJECTS.4ASSET_CRUD.TITLE',
     description: 'PROJECTS.4ASSET_CRUD.DESCRIPTION',
     longDescription: 'PROJECTS.4ASSET_CRUD.LONG_DESCRIPTION',
     link: 'https://github.com/romanostd/4asset-crud'
   },
   {
-    images: ['../assets/terminator1.png', '../assets/terminator2.png', '../assets/terminator3.png'],
+    images: ['assets/terminator1.png', 'assets/terminator2.png', 'assets/terminator3.png'],
     title: 'PROJECTS.TERMINATOR_SUPPLEMENTS.TITLE',
     description: 'PROJECTS.TERMINATOR_SUPPLEMENTS.DESCRIPTION',
     longDescription: 'PROJECTS.TERMINATOR_SUPPLEMENTS.LONG_DESCRIPTION',
     link: 'https://github.com/romanostd/TerminatorSuplements'
   },
   {
-    images: ['../assets/cosmetic1.png', '../assets/cosmetic2.png', '../assets/cosmetic3.png'],
+    images: ['assets/cosmetic1.png', 'assets/cosmetic2.png', 'assets/cosmetic3.png'],
     title: 'PROJECTS.COSMETIC_PAGE.TITLE',
     description: 'PROJECTS.COSMETIC_PAGE.DESCRIPTION',
     longDescription: 'PROJECTS.COSMETIC_PAGE.LONG_DESCRIPTION',
     link: 'https://github.com/romanostd/Cosmetics-page'
   },
   {
-    images: ['../assets/mob1.png'],
+    images: ['assets/mob1.png'],
     title: 'PROJECTS.MOB7_DEV_CHALLENGE.TITLE',
     description: 'PROJECTS.MOB7_DEV_CHALLENGE.DESCRIPTION',
     longDescription: 'PROJECTS.MOB7_DEV_CHALLENGE.LONG_DESCRIPTION',
